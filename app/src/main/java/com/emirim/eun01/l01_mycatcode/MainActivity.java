113f739bb58584c8b1ff88af5437c1ed7e4e4dec
Eun Ju Jhong <eun01300714@gmail.com>
2018-10-18 20:56:40 +0900

    first commit

---
+++ app/src/main/java/com/emirim/eun01/l01_mycatcode/MainActivity.java
@@ -0,0 +1,13 @@
+package com.emirim.eun01.l01_mycatcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
