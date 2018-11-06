fb2ca210fadffec8123d99b69464843b281ca0ac
Eun Ju Jhong <eun01300714@gmail.com>
2018-11-06 20:19:49 +0900

    #10 add Activitys of splash and result

---
+++ app/src/main/java/com/emirim/eun01/l01_mycatcode/ResultActivity.java
@@ -0,0 +1,13 @@
+package com.emirim.eun01.l01_mycatcode;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
